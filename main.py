5c224d54acf7faa54e1888458f0c7dd8ef8dea27
Delpod <xvix56@gmail.com>
2022-11-12 01:57:13 +0100

    Using frequency for words

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from config import frequent_words_file, source_language, target_language, subtit
 con = sqlite3.connect('dictionary.db')
 cur = con.cursor()
 
+con_f = sqlite3.connect('frequent-words.db')
+cur_f = con_f.cursor()
+
 res = cur.execute('SELECT name FROM sqlite_master WHERE type=\'table\' and name=\'known_words\'')
 if res.fetchone() is None:
     cur.execute('CREATE TABLE known_words(word text NOT NULL UNIQUE, translation text NOT NULL, PRIMARY KEY("word"))')
@@ -20,8 +23,26 @@ window = Tk()
 window.geometry("400x500")
 window.title("Subitle Dictionary Translator")
 
-label = Label(window, text="Select words below:", font=("Times New Roman", 10), padx=10, pady=10)
-label.pack()
+label2 = Label(window, text="Don't show most frequent words (0-30000):", font=("Times New Roman", 10), anchor="w")
+label2.pack(padx=10, pady=10, expand=NO, fill=X)
+
+frequency = StringVar()
+text_box = Entry(window, textvariable=frequency)
+text_box.pack(padx=10, pady=10, expand=NO, fill=X)
+
+def FrequencyValidation(S):
+    return re.fullmatch(r'^\d*$', S) != None and (S == '' or (int(S) >= 0 and int(S) <= 30000))
+
+
+vcmd = (text_box.register(FrequencyValidation), '%P')
+text_box.config(validate='key', vcmd=vcmd)
+frequency.set("1000")
+
+btn0 = Button(window, text="Update words")
+btn0.pack(padx=10, pady=10, expand=NO, fill=X)
+
+label = Label(window, text="Select words below:", font=("Times New Roman", 10), anchor="w")
+label.pack(padx=10, pady=10, expand=NO, fill=X)
 listbox = Listbox(window, selectmode="multiple")
 yscrollbar = Scrollbar(listbox, command=listbox.yview)
 yscrollbar.pack(side=RIGHT, fill=Y)
@@ -33,8 +54,6 @@ translate_dict = None
 translate_wordlist = None
 
 def generate_frequent_words_database():
-    con_f = sqlite3.connect('frequent-words.db')
-    cur_f = con_f.cursor()
     cur_f.execute('CREATE TABLE frequent_words(freq integer NOT NULL UNIQUE, word text NOT NULL, PRIMARY KEY("freq"))')
     with open(frequent_words_file) as fwf:
         line = fwf.readline().strip()
@@ -65,6 +84,16 @@ def translate():
         if w in wordlist:
             wordlist.remove(w)
 
+    freq = frequency.get()
+    freq = 0 if freq == '' else int(freq)
+    cur_f.execute(f'SELECT word from frequent_words WHERE freq <= {freq}')
+    freq_words = cur_f.fetchall()
+    freq_words = [w[0] for w in freq_words]
+
+    for w in freq_words:
+        if w in wordlist:
+            wordlist.remove(w)
+
     words = ','.join(["'" + w.replace("'","''") + "'" for w in wordlist])
 
     cur.execute(f'SELECT word, translation from unknown_words WHERE word IN ({words})')
@@ -82,14 +111,13 @@ def translate():
         r = requests.post('https://api-free.deepl.com/v2/translate', headers=headers, params=params)
 
         values = r.json()
-        print(translate_wordlist_joined)
-        print(values)
         translated_words: str = values.get('translations')[0].get('text')
         translated_words = translated_words.rstrip('.').split('. ')
         
         for i, w in enumerate(translate_wordlist):
             translate_dict[w] = translated_words[i]
 
+    listbox.delete(0, listbox.size())
     for w in sorted(list(wordlist)):
         listbox.insert(END, f'{w} ({translate_dict[w]})')
 
@@ -135,17 +163,23 @@ def save_file():
 
 def close():
     con.close()
+    con_f.close()
     window.destroy()
 
 
-translate()
-save_words()
+def translate_and_save():
+    translate()
+    save_words()
 
+
+btn0.config(command=translate_and_save)
 btn1 = Button(window, text="Set as known", command=set_as_known)
 btn1.pack(padx=10, pady=10, expand=NO, fill=X)
 btn2 = Button(window, text="Save file", command=save_file)
 btn2.pack(padx=10, pady=10, expand=NO, fill=X)
 btn3 = Button(window, text="Quit", command=close)
 btn3.pack(padx=10, pady=10, expand=NO, fill=X)
+
+translate_and_save()
 
 window.mainloop()
